6f23de7c5460014fbf08d21ba807a020fd5930de
adahyto <adahy.to@gmail.com>
2019-10-17 22:09:58 +0200

    some style changes

--- src/app/modules/curiosity/curiosity.module.ts
+++ src/app/modules/curiosity/curiosity.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from "@angular/common";
 import { CuriosityComponent } from "./curiosity.component";
 import { CuriosityRoutingModule } from "./curiosity-routing.module";
 import { MatCardModule } from "@angular/material/card";
+import { MatButtonModule } from '@angular/material';
 
 @NgModule({
   declarations: [CuriosityComponent],
-  imports: [CommonModule, CuriosityRoutingModule, MatCardModule]
+  imports: [CommonModule, CuriosityRoutingModule, MatCardModule, MatButtonModule]
 })
 export class CuriosityModule {}
